e16ef7f5bbe204dd29b982060485f42c6f0edf84
trafalgande <thepicvsso@gmail.com>
2021-03-26 02:54:30 +0300

    added jboss

--- src/main/java/se/ifmo/pepe/lab1/Lab1Application.java
+++ src/main/java/se/ifmo/pepe/lab1/Lab1Application.java
@@ -2,9 +2,16 @@ package se.ifmo.pepe.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Lab1Application {
+public class Lab1Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Lab1Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Lab1Application.class, args);
     }
