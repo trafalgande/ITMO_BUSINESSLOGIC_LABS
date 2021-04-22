2a287f1c15876d92d00e63c5091571b7d45ad93a
trafalgande <thepicvsso@gmail.com>
2021-04-23 01:11:03 +0300

    merge

---
+++ clientjs/client.js
@@ -0,0 +1,17 @@
+const SockJS = require('sockjs-client')
+const Stomp = require('stompjs')
+
+let stompClient = null;
+
+function connect() {
+    let socket = new SockJS('/notification')
+    stompClient = Stomp.overWS(socket)
+    stompClient.connect({"Authorization": "Basic bG1hbzpsbWFv"}, function (frame) {
+        console.log('connected ' + frame)
+        stompClient.subscribe('/topic/notification', function (message) {
+            console.log(message)
+        })
+    })
+}
+
+connect()
